53ca235451b10b1a78b4881a760bbf720f002a21
Fabio Ewerton <fabioew89@gmail.com>
2018-01-11 17:04:57 -0400

    atualizacao

--- gulpfile.js
+++ gulpfile.js
@@ -4,39 +4,33 @@ var sass = require('gulp-sass');
 var browserSync = require('browser-sync');
 var del = require('del');
 
-// Move bootstrap
-gulp.task('bootstrap-move', function() {
-    gulp.src('./bower_components/bootstrap/*')
-    .pipe(gulp.dest('./_src/bootstrap/'));
-});
-
 // html
 gulp.task('html', function() {
-    return gulp.src('./_src/*.html')
+    return gulp.src('src/*.html')
       .pipe(html({collapseWhitespace: true}))
-      .pipe(gulp.dest('./_dist/'))
+      .pipe(gulp.dest('dist/'))
       .pipe(browserSync.stream())
 });
 
 // sass
 gulp.task('sass', function() {
-    return gulp.src('./_src/scss/style.scss')
+    return gulp.src('src/scss/style.scss')
       .pipe(sass({outputStyle:"compressed"}))
-      .pipe(gulp.dest('./_dist/css/'))
+      .pipe(gulp.dest('dist/css/'))
       .pipe(browserSync.stream())
 });
 
 gulp.task('BS',['html', 'sass'], function() {
     browserSync.init({
     	server:{
-    		baseDir: "_dist/"
+    		baseDir: "dist/"
     	}
     })
 
     // Observa o html
-    gulp.watch('./_src/*.html', ['html']);
+    gulp.watch('src/*.html', ['html']);
     // Observa o scss
-    gulp.watch('./_src/scss/*.scss', ['sass']);
+    gulp.watch('src/scss/*.scss', ['sass']);
 });
 
 gulp.task('default',['BS']);
