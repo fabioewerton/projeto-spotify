1e676ca459fb736e7be9219c300dff12fe95ed72
Fabio Ewerton <fabioew89@gmail.com>
2018-01-11 17:09:43 -0400

    gulp-notify

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@ var html = require('gulp-htmlmin');
 var sass = require('gulp-sass');
 var browserSync = require('browser-sync');
 var del = require('del');
+var notify = require('gulp-notify');
 
 // html
 gulp.task('html', function() {
@@ -17,7 +18,9 @@ gulp.task('sass', function() {
     return gulp.src('src/scss/style.scss')
       .pipe(sass({outputStyle:"compressed"}))
       .pipe(gulp.dest('dist/css/'))
+      .on("error", notify.onError("Error: <%= error.message %>"))
       .pipe(browserSync.stream())
+
 });
 
 gulp.task('BS',['html', 'sass'], function() {
@@ -27,9 +30,7 @@ gulp.task('BS',['html', 'sass'], function() {
     	}
     })
 
-    // Observa o html
     gulp.watch('src/*.html', ['html']);
-    // Observa o scss
     gulp.watch('src/scss/*.scss', ['sass']);
 });
 
